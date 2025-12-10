ef0210c106e83ae6b84ac764e001732f867c54f8
Taman Hassan <tamanhassan1@gmail.com>
2025-12-10 13:24:33 +0100

    updated game.jsx code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/home/Home';
-import Game from './pages/game/Game';
+import Game from './pages/game/Game.jsx';
 
 function App() {
 
