2cb922a2be7ab98b5e30f451e3d7512f52b2ff58
LoS <lostreit@pm.me>
2026-01-05 11:16:30 +0100

    fixed signInUp issues

--- src/components/SignInUp/SignInUp.jsx
+++ src/components/SignInUp/SignInUp.jsx
@@ -5,95 +5,125 @@ import { useNavigate } from 'react-router-dom' // malin
 function SignInUp() {
     const [action, setAction] = useState("Sign Up")
 
+   
 
-    const [email, setEmail] = useState(" ")
-    const [pwd, setPwd] = useState(" ")
+    const [signUpMail, setSignUpMail] = useState("")
+    const [signInMail, setSignInMail] = useState("")
+    const [signUpPwd, setSignUpPwd] = useState("")
+    const [signInPwd, setSignInPwd] = useState("")
+    
+    // adding navigation to character
+    const navigate = useNavigate();
 
     const addStorage = () => {
-        localStorage.setItem("email", email)
-        localStorage.setItem("pwd", pwd)
-        setEmail(" ")
-        setPwd(" ")
+        localStorage.setItem("email", signUpMail.trim())
+        localStorage.setItem("pwd", signUpPwd)
+        setSignUpMail("")
+        setSignUpPwd("")
     }
 
     const signIn = () => {
         const userEmail = localStorage.getItem("email")
         const userPwd = localStorage.getItem("pwd")
-        email === userEmail && pwd === userPwd
-            ? alert("You are Signed In !")
-            : alert("Incorrect email or password..")
+
+        if (signInMail.trim() === userEmail && signInPwd === userPwd) {
+            alert("You are Signed In !")
+            navigate("/opponents") 
+        } else {
+            alert("Incorrect email or password..") 
+        }
     }
 
-    // adding navigation to character /Malin
-    const navigate = useNavigate();
+    const switchMode = (mode) => {
+        setAction(mode)
+        setSignUpMail("")
+        setSignInMail("")
+        setSignUpPwd("")
+        setSignInPwd("")
+    }
+
+    const isSignUp = action === "Sign Up"
 
     return (
 
         <div className={style.container}>
             <h1>{action}</h1>
-
-            <div className={style.container__input}>
-                {action === "Sign Up" && (
-                    <div className={style.email}>
-                        <input
-                            type="email"
-                            placeholder="Email..."
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <br />
-                        <br />
-                        <input
-                            type="password"
-                            placeholder="Choose a Password..."
-                            value={pwd}
-                            onChange={(e) => setPwd(e.target.value)}
-                        />
-                    </div>
-                )}
-
-                {action === "Sign In" && (
-                    <div className={style.email}>
-                        <input
-                            type="email"
-                            placeholder="Email..."
-                        />
-                        <br />
-                        <br />
-                        <input
-                            type="password"
-                            placeholder="Your Password..."
-                        />
-                    </div>
-                )}
-            </div>
-
-
-            <button
-                onClick={() => {
-                    if (action === "Sign In") {
-                        signIn()
-                    } else {
+        
+            <form 
+                className={style.container__input}
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    if (isSignUp)
                         addStorage()
-                    }
+                    else
+                        signIn()
                 }}
-            >Submit
-            </button>
+            >
+
+            {isSignUp ? (
+                <div className={style.email}>
+                    <input
+                        type="email"
+                        placeholder="Email..."
+                        value={signUpMail}
+                        onChange={(e) => setSignUpMail(e.target.value)}
+                        autoComplete='email'
+                    />
+                    <br />
+                    <br />
+                    <input
+                        type="password"
+                        placeholder="Choose a Password..."
+                        value={signUpPwd}
+                        onChange={(e) => setSignUpPwd(e.target.value)}
+                        autoComplete='new-password'
+                    />
+                </div>
+            ) : (
+                <div className={style.email}>
+                    <input
+                        type="email"
+                        placeholder="Email..."
+                        value={signInMail}
+                        onChange={(e) => setSignInMail(e.target.value)}
+                        autoComplete='username'
+                    />
+                    <br />
+                    <br />
+                    <input
+                        type="password"
+                        placeholder="Your Password..."
+                        value={signInPwd}
+                        onChange={(e) => setSignInPwd(e.target.value)}
+                        autoComplete='current-password'
+                    />
+                </div>
+            )}
+            
+            <button type='submit'>Submit</button>
+        </form>
+
             <div className={style.container__btn}>
-                <button className={
-                    action === "Sign In" ? style.white : style.submit} onClick={() => {
-                    setAction("Sign In")
-                    navigate("/opponents") //Malin
+                <button 
+                    className={
+                    action === "Sign In" ? style.white : style.submit}
+                    type='button'
+                    onClick={() => {
+                    switchMode("Sign In")
                 }}>
                     Sign In
                 </button>
-                <button className={action === "Sign Up" ? style.white : style.submit} onClick={() => {
-                    setAction("Sign Up")
+
+                <button 
+                className={
+                    action === "Sign Up" ? style.white : style.submit} 
+                    type='button'
+                    onClick={() => {
+                    switchMode("Sign Up")
                 }}>
                     Sign Up
                 </button>
             </div>
-
         </div>
     )
 }
